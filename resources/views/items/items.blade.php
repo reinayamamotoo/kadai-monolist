ab4c60acf1f5d7bfc79b4be1e53fc83e17f1014a
jiro.kirameki <ec2-user@ip-172-31-40-226.us-east-2.compute.internal>
2018-06-21 07:49:02 +0000

    havewant

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -22,8 +22,8 @@
                         </div>
                         @if (isset($item->count))
                             <div class="panel-footer">
-                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Wants</p>
-                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Has</p>
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} {{$name}} </p>
+                                
                             </div>
                         @endif
                     </div>
